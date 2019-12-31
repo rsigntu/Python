f5e338fc78dc6031b3a595bce804d2dd9626b8fe
Ravi Singh <rsigntu@gmail.com>
2019-12-31 16:59:23 +0530

    1st commit+2nd commit + 3rd commit+4th commit

--- syntaxError.py
+++ syntaxError.py
@@ -3,3 +3,7 @@ print(p)
 print("hello to git sassa")
 print("line is added to master branch only")
 print("line is addded to test rebase with master")
+print("hello to git sassa one more branch is added to track master")
+print("second line print")
+print("this is third line added in third commit");
+print("line  is added to merge previous commit with local changes")
